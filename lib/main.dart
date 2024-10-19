cecb18298445e4219fb3b16fcad9e6f2cdee3246
Ravi Shankar Pradhan <ravishankarpradhan76@gmail.com>
2024-10-20 00:58:57 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/pages/home_screen1.dart';
+import 'package:flutter_app/pages/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:HomeScreen1(),
+      home:HomeScreen(),
     );
   }
 }
